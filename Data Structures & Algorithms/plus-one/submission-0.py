b7d24d7515883292429104e61e4997ae653d8d7c
Bhanu <99273450+Bhanu-goel@users.noreply.github.com>
2026-04-15 13:02:40 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,15 @@
+def digi(num):
+    ans = []
+    while num>0:
+        mod = num%10
+        ans.append(mod)
+        num//=10
+    return ans[::-1]
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = [str(i) for i in digits]
+        num = int(''.join(digits))
+        num += 1
+        digits = digi(num)
+        return digits
